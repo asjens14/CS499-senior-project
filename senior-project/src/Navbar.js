d28d5ed361281abf18cb82f991cbd8bd13c86df4
asjens14 <56127260+asjens14@users.noreply.github.com>
2023-11-13 17:01:31 -0700

    Update Navbar.js

--- senior-project/src/Navbar.js
+++ senior-project/src/Navbar.js
@@ -4,9 +4,10 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Spending Go Up</h1>
-      <div>
-        <a>State</a>
+      <div>        
         <Link to='state-compare'>State Compare</Link>
+        <Link to='year-compare'>Year Compare</Link>
+        <a href="">More</a>
         <Link to="/about">About</Link>
 
       </div>
