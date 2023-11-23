eb5d2f01c98eb64c63aebdc15c5e686c97c91610
asjens14 <56127260+asjens14@users.noreply.github.com>
2023-11-22 23:30:44 -0700

    Update Navbar.js

--- senior-project/src/Navbar.js
+++ senior-project/src/Navbar.js
@@ -3,12 +3,12 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Spending Go Up</h1>
-      <div>        
+      <h1><Link to='/'>Spending Go Up</Link></h1>
+        <Link to="/about">About</Link>
+      <div className='dropdown'>        
         <Link to='state-compare'>State Compare</Link>
         <Link to='year-compare'>Year Compare</Link>
         <a>More</a>
-        <Link to="/about">About</Link>
 
       </div>
     </nav>
