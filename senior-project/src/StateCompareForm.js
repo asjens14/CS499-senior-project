ceeb3d4d494e6215a35366d4f246e2c3586a1d0e
asjens14 <56127260+asjens14@users.noreply.github.com>
2023-11-22 23:30:37 -0700

    Update StateCompareForm.js

--- senior-project/src/StateCompareForm.js
+++ senior-project/src/StateCompareForm.js
@@ -1,195 +1,225 @@
-import { useState } from "react";
-import Graph from './Graph';
-
+import { useEffect, useState } from "react";
+import StackedGraph from './StackedGraph';
+// FIX TO USE STACKED CHART AND SHOW ALL YEARS
 
 const StateCompareForm = () => {
-  const [state1, setState1] = useState('49');
-  const [state2, setState2] = useState('41');
-  const [year, setYear] = useState('2023');
+  const [state1,setState1] = useState('AK')
+  const [state2,setState2] = useState('AL')
+  const [ndata,setData] = useState([
+    {val:'2008',AK:4123874681.64,AL:23320411271.43},
+    {val:'2009',AK:6640048390.84,AL:36655099194.03},
+    {val:'2010',AK:6545982736.8,AL:34518005926.22},
+    {val:'2011',AK:6517527426.92,AL:46146922481.25},
+    {val:'2012',AK:6864100069.5,AL:44392058606.98},
+    {val:'2013',AK:6035327242.2,AL:43780497389.79},
+    {val:'2014',AK:6898289232.08,AL:35802762611.9},
+    {val:'2015',AK:7290975135.11,AL:59726230305.32},
+    {val:'2016',AK:7331885742.97,AL:64407468332.37},
+    {val:'2017',AK:8811651202.85,AL:67359892240.39},
+    {val:'2018',AK:9809112430.4,AL:57544145086.85},
+    {val:'2019',AK:10338318244.66,AL:52730877880.11},
+    {val:'2020',AK:16667311410.84,AL:63439234896.31},
+    {val:'2021',AK:18620867062.37,AL:70029574841.81},
+    {val:'2022',AK:15028929814.69,AL:60835762751.61},
+    {val:'2023',AK:15615772325.36,AL:62132556180.27},
+    {val:'2024',AK:1783464599.19,AL:4864472265.49},
+  ])
 
-  let url1 = 'https://api.usaspending.gov/api/v2/recipient/state/49/?year=2023'
-  let url2 = 'https://api.usaspending.gov/api/v2/recipient/state/41/?year=2023'
-
-  const [data, setData] = useState([
-    {val:'OR', num:48271413433.72},
-    {val:'UT',num:24440930738.44}
-  ])  
-
-  const handleSubmit = (e) => {
+  async function getData(e){
     e.preventDefault();
-    let nData = []
-    const nUrl1 = 'https://api.usaspending.gov/api/v2/recipient/state/' + state1 + '/?year=' + year
-    const nUrl2 = 'https://api.usaspending.gov/api/v2/recipient/state/' + state2 + '/?year=' + year
-      url1 = nUrl1;
-      url2 = nUrl2;
-    // first set of data
-    fetch(url1)
-    .then((response) => response.json())
-    .then((json) => {
-      const k =json['name']
-      const v = json['total_prime_amount']
-      nData.push({val:k,num:v})
-    })
-    .catch((error) => console.error(`Error fetching data: ${error.message}`));
-    // second set of data
-    fetch(url2)
-    .then((response) => response.json())
-    .then((json) => {
-      const k =json['name']
-      const v = json['total_prime_amount']
-      nData.push({val:k,num:v})
-    })
-    .catch((error) => console.error(`Error fetching data: ${error.message}`));
+    console.log('clicked')
+    let data1=[];
+    let data2=[];
+    let temp=[]
 
-    // that sweet, sweet jank, because it doesnt update immediately, and I don't
-    // know how to fix it. For now, this works
-    setTimeout(()=> {
-    setData(nData)
+    await fetch('https://api.usaspending.gov/api/v2/search/spending_over_time/',{
+        method:'POST',
     
-    },1000)
+        headers:{
+            'Accept':'application/json',
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+            "group": "fiscal_year",
+            "filters": {
+                "place_of_performance_locations":[{"country":"USA","state":state1}],
+                "time_period":[{"start_date":"2007-10-01","end_date":"2024-09-30"}]
+            }
+        })})
+      .then((response) => response.json())
+      .then((json) => {
+        // TODO: parse data
+        
+        let res = json['results'];
+        for (let i = 0;i < res.length;i++){
+          // console.log(res[i])
+          const k = res[i]['time_period']['fiscal_year']
+          const v = res[i]['aggregated_amount']
+          data1.push({val:k,num:v})
+        }
+        
+      })
+      .catch((error) => console.error(`Error fetching data: ${error.message}`));
+
+    await fetch('https://api.usaspending.gov/api/v2/search/spending_over_time/',{
+      method:'POST',    
+      headers:{
+          'Accept':'application/json',
+          'Content-Type': 'application/json'
+      },
+      body:JSON.stringify({
+          "group": "fiscal_year",
+          "filters": {
+              "place_of_performance_locations":[{"country":"USA","state":state2}],
+              "time_period":[{"start_date":"2007-10-01","end_date":"2024-09-30"}]
+          }
+      })})
+      .then((response) => response.json())
+      .then((json) => {
+      // TODO: parse data
+      
+        let res = json['results'];
+        for (let i = 0;i < res.length;i++){
+          // console.log(res[i])
+          const k = res[i]['time_period']['fiscal_year']
+          const v = res[i]['aggregated_amount']
+          data2.push({val:k,num:v})
+        }
+      
+      })
+      .catch((error) => console.error(`Error fetching data: ${error.message}`));
+
+    // combine data into the correct format for the graph
+    
+    for (let i = 0; i < data1.length;i++){
+      temp.push({val:data1[i]['val'],[state1]:data1[i]['num'],[state2]:data2[i]['num']})
+    }
+    setData(temp)
+    console.log(ndata)
   }
+  
+  useEffect(()=>{
+    console.log(ndata)
+    console.log('set data')
+  },[ndata])
 
   
   return ( 
     <div>
       <div className="form">  
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={getData}>
           <label>State 1</label>
           <select name="state1" value={state1} onChange={(e) => setState1(e.target.value)}>
-            <option value="02">AK</option>
-            <option value="01">AL</option>
-            <option value="05">AR</option>
-            <option value="04">AZ</option>
-            <option value="06">CA</option>
-            <option value="08">CO</option>
-            <option value="09">CT</option>
-            <option value="10">DE</option>
-            <option value="12">FL</option>
-            <option value="13">GA</option>
-            <option value="15">HI</option>
-            <option value="19">IA</option>
-            <option value="16">ID</option>
-            <option value="17">IL</option>
-            <option value="18">IN</option>
-            <option value="20">KS</option>
-            <option value="21">KY</option>
-            <option value="22">LA</option>
-            <option value="25">MA</option>
-            <option value="24">MD</option>
-            <option value="23">ME</option>
-            <option value="26">MI</option>
-            <option value="27">MN</option>
-            <option value="29">MO</option>
-            <option value="28">MS</option>
-            <option value="30">MT</option>
-            <option value="37">NC</option>
-            <option value="38">ND</option>
-            <option value="31">NE</option>
-            <option value="33">NH</option>
-            <option value="34">NJ</option>
-            <option value="35">NM</option>
-            <option value="32">NV</option>
-            <option value="36">NY</option>
-            <option value="39">OH</option>
-            <option value="40">OK</option>
-            <option value="41">OR</option>
-            <option value="42">PA</option>
-            <option value="44">RI</option>
-            <option value="45">SC</option>
-            <option value="46">SD</option>
-            <option value="47">TN</option>
-            <option value="48">TX</option>
-            <option value="49">UT</option>
-            <option value="51">VA</option>
-            <option value="50">VT</option>
-            <option value="53">WA</option>
-            <option value="55">WI</option>
-            <option value="54">WV</option>
-            <option value="56">WY</option>
+            <option value="AK">AK</option>
+            <option value="AL">AL</option>
+            <option value="AR">AR</option>
+            <option value="AZ">AZ</option>
+            <option value="CA">CA</option>
+            <option value="CO">CO</option>
+            <option value="CT">CT</option>
+            <option value="DE">DE</option>
+            <option value="FL">FL</option>
+            <option value="GA">GA</option>
+            <option value="HI">HI</option>
+            <option value="IA">IA</option>
+            <option value="ID">ID</option>
+            <option value="IL">IL</option>
+            <option value="IN">IN</option>
+            <option value="KS">KS</option>
+            <option value="KY">KY</option>
+            <option value="LA">LA</option>
+            <option value="MA">MA</option>
+            <option value="MD">MD</option>
+            <option value="ME">ME</option>
+            <option value="MI">MI</option>
+            <option value="MN">MN</option>
+            <option value="MO">MO</option>
+            <option value="MS">MS</option>
+            <option value="MT">MT</option>
+            <option value="NC">NC</option>
+            <option value="ND">ND</option>
+            <option value="NE">NE</option>
+            <option value="NH">NH</option>
+            <option value="NJ">NJ</option>
+            <option value="NM">NM</option>
+            <option value="NV">NV</option>
+            <option value="NY">NY</option>
+            <option value="OH">OH</option>
+            <option value="OK">OK</option>
+            <option value="OR">OR</option>
+            <option value="PA">PA</option>
+            <option value="RI">RI</option>
+            <option value="SC">SC</option>
+            <option value="SD">SD</option>
+            <option value="TN">TN</option>
+            <option value="TX">TX</option>
+            <option value="UT">UT</option>
+            <option value="VA">VA</option>
+            <option value="VT">VT</option>
+            <option value="WA">WA</option>
+            <option value="WI">WI</option>
+            <option value="WV">WV</option>
+            <option value="WY">WY</option>
           </select>
             
           <label>State 2</label>
           <select name="state2" value={state2} onChange={(e) => setState2(e.target.value)}>
-            <option value="02">AK</option>
-            <option value="01">AL</option>
-            <option value="05">AR</option>
-            <option value="04">AZ</option>
-            <option value="06">CA</option>
-            <option value="08">CO</option>
-            <option value="09">CT</option>
-            <option value="10">DE</option>
-            <option value="12">FL</option>
-            <option value="13">GA</option>
-            <option value="15">HI</option>
-            <option value="19">IA</option>
-            <option value="16">ID</option>
-            <option value="17">IL</option>
-            <option value="18">IN</option>
-            <option value="20">KS</option>
-            <option value="21">KY</option>
-            <option value="22">LA</option>
-            <option value="25">MA</option>
-            <option value="24">MD</option>
-            <option value="23">ME</option>
-            <option value="26">MI</option>
-            <option value="27">MN</option>
-            <option value="29">MO</option>
-            <option value="28">MS</option>
-            <option value="30">MT</option>
-            <option value="37">NC</option>
-            <option value="38">ND</option>
-            <option value="31">NE</option>
-            <option value="33">NH</option>
-            <option value="34">NJ</option>
-            <option value="35">NM</option>
-            <option value="32">NV</option>
-            <option value="36">NY</option>
-            <option value="39">OH</option>
-            <option value="40">OK</option>
-            <option value="41">OR</option>
-            <option value="42">PA</option>
-            <option value="44">RI</option>
-            <option value="45">SC</option>
-            <option value="46">SD</option>
-            <option value="47">TN</option>
-            <option value="48">TX</option>
-            <option value="49">UT</option>
-            <option value="51">VA</option>
-            <option value="50">VT</option>
-            <option value="53">WA</option>
-            <option value="55">WI</option>
-            <option value="54">WV</option>
-            <option value="56">WY</option>
-          </select>
-            
-          <label>Year</label>
-          <select
-            name="year"
-            value={year}
-            onChange={(e) => setYear(e.target.value)} >
-            <option value="2008">2008</option>
-            <option value="2009">2009</option>
-            <option value="2010">2010</option>
-            <option value="2011">2011</option>
-            <option value="2012">2012</option>
-            <option value="2013">2013</option>
-            <option value="2014">2014</option>
-            <option value="2015">2015</option>
-            <option value="2016">2016</option>
-            <option value="2017">2017</option>
-            <option value="2018">2018</option>
-            <option value="2019">2019</option>
-            <option value="2020">2020</option>
-            <option value="2021">2021</option>
-            <option value="2022">2022</option>
-            <option value="2023">2023</option>
-            <option value="2024">2024</option>
+            <option value="AK">AK</option>
+            <option value="AL">AL</option>
+            <option value="AR">AR</option>
+            <option value="AZ">AZ</option>
+            <option value="CA">CA</option>
+            <option value="CO">CO</option>
+            <option value="CT">CT</option>
+            <option value="DE">DE</option>
+            <option value="FL">FL</option>
+            <option value="GA">GA</option>
+            <option value="HI">HI</option>
+            <option value="IA">IA</option>
+            <option value="ID">ID</option>
+            <option value="IL">IL</option>
+            <option value="IN">IN</option>
+            <option value="KS">KS</option>
+            <option value="KY">KY</option>
+            <option value="LA">LA</option>
+            <option value="MA">MA</option>
+            <option value="MD">MD</option>
+            <option value="ME">ME</option>
+            <option value="MI">MI</option>
+            <option value="MN">MN</option>
+            <option value="MO">MO</option>
+            <option value="MS">MS</option>
+            <option value="MT">MT</option>
+            <option value="NC">NC</option>
+            <option value="ND">ND</option>
+            <option value="NE">NE</option>
+            <option value="NH">NH</option>
+            <option value="NJ">NJ</option>
+            <option value="NM">NM</option>
+            <option value="NV">NV</option>
+            <option value="NY">NY</option>
+            <option value="OH">OH</option>
+            <option value="OK">OK</option>
+            <option value="OR">OR</option>
+            <option value="PA">PA</option>
+            <option value="RI">RI</option>
+            <option value="SC">SC</option>
+            <option value="SD">SD</option>
+            <option value="TN">TN</option>
+            <option value="TX">TX</option>
+            <option value="UT">UT</option>
+            <option value="VA">VA</option>
+            <option value="VT">VT</option>
+            <option value="WA">WA</option>
+            <option value="WI">WI</option>
+            <option value="WV">WV</option>
+            <option value="WY">WY</option>
           </select>
           <button>Submit</button>
         </form>
       </div>
       <div className='content'>
-        {<Graph data={data} width={500}/>}
+        {<StackedGraph data={ndata} width={1000} s1={state1} s2={state2}/>}
       </div>
     </div>
    );
