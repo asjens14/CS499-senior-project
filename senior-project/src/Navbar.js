8cf389862cf8b9040043b91b19bbec857446a1a4
asjens14 <56127260+asjens14@users.noreply.github.com>
2023-12-06 14:42:55 -0700

    Update Navbar.js
    add mobile view

--- senior-project/src/Navbar.js
+++ senior-project/src/Navbar.js
@@ -1,17 +1,56 @@
-import {Link} from 'react-router-dom'
-
+import {Link} from 'react-router-dom';
+import Media from 'react-media'
+import { useState } from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+// max-width:420
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <h1><Link to='/'>Spending Go Up</Link></h1>
-        <Link to="/about">About</Link>
-      <div className='dropdown'>        
-        <Link to='state-compare'>State Compare</Link>
-        <Link to='year-compare'>Year Compare</Link>
-        <a>More</a>
+  const [isCollapsed, setIsCollapsed] = useState(true)
 
-      </div>
-    </nav>
+  const toggleClass = () =>{
+    setIsCollapsed(!isCollapsed)
+  }
+  return (
+    <div>
+      <Media queries={{ small: "(max-width:470px)" }}>
+        {matches =>
+        matches.small ? (
+          <nav className='topnav'>
+            <Link to='/'><h1>Spending Go Up</h1></Link>
+            <div className="myLinks">{/*myLinks*/}
+              <div className={isCollapsed ? 'mobile-dropdown-content collapsed':'mobile-dropdown-content displayed'}>        
+                <Link to="/about">About</Link>
+                <Link to='state-compare'>State Compare</Link>
+                <Link to='year-compare'>Spending over time</Link>
+                <Link to='debt'>Debt</Link>
+                {/* <Link to='year-compare'>download</Link> */}
+                <Link to="agency-spending">Agencies</Link>
+                {/* <a href="">Disaster</a> */}
+              </div>
+              <a onClick={toggleClass} className='icon'><FontAwesomeIcon icon={faBars} /></a>{/*icon*/}
+            </div>            
+          </nav>
+        ) : (
+          <nav className="navbar">
+            <Link to='/'><h1>Spending Go Up</h1></Link>
+              <Link to="/about">About</Link>
+              <div className="dropdown">
+                <button className='dropbtn'>More</button>
+                <div className='dropdown-content'>        
+                  <Link to='state-compare'>State Compare</Link>
+                  <Link to='states-by-year'>States by year</Link>
+                  <Link to='year-compare'>Spending over time</Link>
+                  <Link to='debt'>Debt</Link>
+                  {/* <Link to='year-compare'>download</Link> */}
+                  <Link to="agency-spending">Agencies</Link>
+                  {/* <a href="disaster-spending">Disaster</a> */}
+                </div>
+              </div>
+          </nav>
+        )}
+
+      </Media>
+    </div>
   );
 }
  
