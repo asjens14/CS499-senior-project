cc5470aeb9cd78a52fc5a74656a09c3cac3b1b2c
asjens14 <56127260+asjens14@users.noreply.github.com>
2024-02-03 23:53:36 -0700

    Update AboutPage.js

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -6,7 +6,7 @@ const AboutPage = () => {
         This project was created for my senior project
         It utilizes data from the USASpending API
       </p>
-      <span>Code can be found <a href="/about">here</a></span>
+      <span>Code can be found <a href="https://github.com/asjens14/CS499-senior-project">here</a></span>
     </div>
   );
 }
